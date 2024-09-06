1c480af9fc510d43a4cb27cb109d63318a56294d
Manuel Maier <maiermanuel423@gmail.com>
2024-09-06 14:10:59 +0200

    Encounter aus dem Unterricht 6.9.

--- fhirstarter/src/main/java/at/spengergasse/fhirstarter/entity/Encounter.java
+++ fhirstarter/src/main/java/at/spengergasse/fhirstarter/entity/Encounter.java
@@ -1,13 +1,17 @@
 package at.spengergasse.fhirstarter.entity;
 
 import at.spengergasse.fhirstarter.model.DomainResource;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import at.spengergasse.fhirstarter.model.Identifier;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "en_encounter")
@@ -17,4 +21,15 @@ import lombok.experimental.SuperBuilder;
 @Setter
 @SuperBuilder
 public class Encounter extends DomainResource {
+
+    public enum Statuscode {planned, arrived, triaged, @JsonProperty("in-progress") in_progress, onleave, finished, cancelled};
+
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "en_id_identifier")
+    private List<Identifier> identifiers = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "en_status")
+    private Statuscode status;
 }
